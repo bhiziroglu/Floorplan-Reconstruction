5db862415c8378574ca78298ac1d9a4b8c26a129
Berkan <bhiziroglu14@ku.edu.tr>
2018-08-02 15:32:08 +0300

    Minor fix.

--- source.py
+++ source.py
@@ -11,9 +11,9 @@ import config as cfg
 
 
 def euclidian_distance(p1,p2):
-    x1,y1 = p1
-    x2,y2 = p2
-    return math.sqrt(math.pow(x1-x2,2) + math.pow(y1-y2,2))
+    x1, y1 = p1
+    x2, y2 = p2
+    return math.sqrt(math.pow(x1-x2, 2) + math.pow(y1-y2, 2))
 
 
 def show(im):
@@ -164,8 +164,8 @@ def thick_line(x1,y1,x2,y2):
     if (y2-y1)/(x2-x1)<1:
         wy=(thickness-1)*math.sqrt(pow((x2-x1),2)+pow((y2-y1),2))/(2*math.fabs(x2-x1));
         for i in range(int(wy)+1):
-            res += bline(x1,y1-i,x2,y2-i);
-            res += bline(x1,y1+i,x2,y2+i);
+            res += bline(x1,y1-i,x2,y2-i)
+            res += bline(x1,y1+i,x2,y2+i)
     else:
         wx=(thickness-1)*math.sqrt(pow((x2-x1),2)+pow((y2-y1),2))/(2*math.fabs(y2-y1));
         for i in range(int(wx)+1):
@@ -180,10 +180,11 @@ def add_border(src):
     bottom = int(0.04 * src.shape[0])*0+50
     left = int(0.04 * src.shape[1])*0+50
     right = int(0.04 * src.shape[1])*0+50
-    res = cv2.copyMakeBorder(src, top, bottom, left, right, cv2.BORDER_CONSTANT, value=[255,255,255])
+    res = cv2.copyMakeBorder(src, top, bottom, left, right, cv2.BORDER_CONSTANT, value=[255, 255, 255])
     return res
 
 def add_border_binary(src):
+
     top = int(0.04 * src.shape[0])*0+50
     bottom = int(0.04 * src.shape[0])*0+50
     left = int(0.04 * src.shape[1])*0+50
@@ -220,80 +221,77 @@ img2 = add_border(img)
 
 kernel = np.ones(cfg.CLOSE_KERNEL_SIZE, np.uint8)
 closing = cv2.morphologyEx(img2, cv2.MORPH_CLOSE, kernel)
-gray = cv2.cvtColor(closing,cv2.COLOR_BGR2GRAY)
+gray = cv2.cvtColor(closing, cv2.COLOR_BGR2GRAY)
 
 
-T = cv2.imread(cfg.TARGET_NAME,cv2.IMREAD_GRAYSCALE)
+T = cv2.imread(cfg.TARGET_NAME, cv2.IMREAD_GRAYSCALE)
 T = add_border_binary(T)
 
 kernel2 = np.ones(cfg.DILATE_KERNEL_SIZE, np.uint8)
 T = cv2.morphologyEx(T, cv2.MORPH_DILATE, kernel2,iterations=2) # For ground truth checking
 
 gray = np.float32(gray)
-dst = cv2.cornerHarris(gray,cfg.HARRIS_BLOCK_SIZE,cfg.HARRIS_K_SIZE,cfg.HARRIS_K)
-dst = cv2.dilate(dst,None)
+dst = cv2.cornerHarris(gray, cfg.HARRIS_BLOCK_SIZE, cfg.HARRIS_K_SIZE, cfg.HARRIS_K)
+dst = cv2.dilate(dst, None)
 
 n = np.zeros_like(img2)
 n[dst > cfg.HARRIS_QUALITY * dst.max()] = [255,255,255]
 
 '''Get the corner coordinates'''
-m = n[:,:,0] # Manual RGB to Binary
+m = n[:, :, 0] # Manual RGB to Binary
 cornerList = []
 for i in range(m.shape[0]):
     for j in range(m.shape[1]):
-        if(m[i][j]==255): # CORNER FOUND !
-            cornerList.append((i,j))
+        if m[i][j] == 255:  # CORNER FOUND !
+            cornerList.append((i, j))
 
 
-reducedCorners = corner_reducer(cornerList,cfg.REDUCTION_DISTANCE_1) # 20 is the Treshold for the max distance between any two vertices to treat them as one.
+reducedCorners = corner_reducer(cornerList, cfg.REDUCTION_DISTANCE_1)  # 20 is the Treshold for the max distance between any two vertices to treat them as one.
 
-snapped = snap_to_grid(reducedCorners,cfg.GRID_SIZE)
+snapped = snap_to_grid(reducedCorners, cfg.GRID_SIZE)
 
-snapped = corner_reducer(snapped,cfg.REDUCTION_DISTANCE_2) # Reduce again to dilate grouped up points
-
-
+snapped = corner_reducer(snapped, cfg.REDUCTION_DISTANCE_2)  # Reduce again to dilate grouped up points
 
 proximity = proximity_matrix(snapped)
 
-if cfg.PIXEL_SHIFT>0:
+if cfg.PIXEL_SHIFT > 0:
     snapped_updated = []
     for p in snapped:
-        snapped_updated.append((p[0],p[1]-5)) #Shift every corner 5 pixels up !
+        snapped_updated.append((p[0], p[1]-5)) #Shift every corner 5 pixels up !
     snapped = snapped_updated
 
 
 corners = {}
-hash_ = lambda V : V[0]*V[1]+V[0]+V[1]
+hash_ = lambda V: V[0]*V[1]+V[0]+V[1]
 
 
-S = findsubsets(snapped,2) # N * (N-1) / 2 items
+S = findsubsets(snapped, 2)  # N * (N-1) / 2 items
 
 walls = []
 seen = []
 TOTAL = len(S)
-skipCounter =0
+skipCounter = 0
 iteration = 0
+
 for pair in iter(S):
     iteration += 1
-    if(iteration%50==0):
+    if iteration % 50 == 0:
         print("Completed %"+str(iteration*100.0/TOTAL))
 
     p1, p2 = pair
 
-    if proximity[snapped.index(p1),snapped.index(p2)] == 0: #Skip if same digit
+    if proximity[snapped.index(p1), snapped.index(p2)] == 0: #Skip if same digit
         continue
 
-    if (p1,p2) in seen or (p2,p1) in seen: # Do not add bi-directional walls
+    if (p1, p2) in seen or (p2, p1) in seen: # Do not add bi-directional walls
         continue
 
-    ends = np.array([[ p1[0], p1[1]],
-                 [ p2[0], p2[1]]])
+    ends = np.array([[p1[0], p1[1]],
+                 [p2[0], p2[1]]])
 
-
-    if int(ends[0][0]) - int(ends[1][0]) == 0: #or ends[0][1]-ends[1][1] == 0:
+    if int(ends[0][0]) - int(ends[1][0]) == 0:
 
         points = thick_line_fixed_x(int(p1[0]), int(p1[1]), int(p2[0]), int(p2[1]))
-
 
     elif int(ends[0][1]) - int(ends[1][1]) == 0:
 
@@ -305,25 +303,24 @@ for pair in iter(S):
 
     cntr = 0
 
-
     for p in points:
 
-        if(p[0]>=T.shape[0]):
-            skipCounter +=1
+        if p[0] >= T.shape[0]:
+            skipCounter += 1
             continue
-        if(p[1]>=T.shape[1]):
-            skipCounter +=1
+        if p[1] >= T.shape[1]:
+            skipCounter += 1
             continue
 
-        if T[int(p[0]),int(p[1])]==255:
-            cntr+=1
+        if T[int(p[0]), int(p[1])] == 255:
+            cntr += 1
 
     alpha = cfg.ALPHA
     beta = cfg.BETA
 
-    rank = alpha*(cntr/len(points)) + beta * (1/proximity[snapped.index(p1),snapped.index(p2)])
+    rank = alpha*(cntr/len(points)) + beta * (1/proximity[snapped.index(p1), snapped.index(p2)])
 
-    if(rank > cfg.RANK_THRESHOLD):
+    if rank > cfg.RANK_THRESHOLD:
 
         walls.append({
             'corner1': str(int(hash_(p1))),
@@ -331,7 +328,7 @@ for pair in iter(S):
             'frontTexture': {},
             'backTexture': {}
         })
-        seen.append((p1,p2))
+        seen.append((p1, p2))
 
 
 print("Completed %100")
@@ -349,10 +346,10 @@ reduced_walls = []
 seen = []
 for wall in walls:
 
-    if (wall['corner1'],wall['corner2']) in seen or (wall['corner2'],wall['corner1']) in seen:
+    if (wall['corner1'], wall['corner2']) in seen or (wall['corner2'], wall['corner1']) in seen:
         continue
 
-    seen.append((wall['corner1'],wall['corner2']))
+    seen.append((wall['corner1'], wall['corner2']))
 
     reduced_walls.append({
         'corner1': wall['corner1'],
@@ -362,7 +359,7 @@ for wall in walls:
     })
 
 
-if cfg.REMOVE_COLLISION==True:
+if cfg.REMOVE_COLLISION == True:
     reduced_walls_2 = []
     skipList = []
     willRemove = []
@@ -376,7 +373,7 @@ if cfg.REMOVE_COLLISION==True:
                 corner1 = corners[wall['corner1']]
                 corner2 = corners[wall['corner2']]
 
-                if i == 0: # To generate permutations for corner1,corner2 pairs for each edge.
+                if i == 0:  # To generate permutations for corner1,corner2 pairs for each edge.
 
                     corner1_X = corner1['x']
                     corner1_Y = corner1['y']
@@ -397,12 +394,8 @@ if cfg.REMOVE_COLLISION==True:
                     if wall2 in seen:
                         continue
 
-                    #if wall['corner1'] == wall2['corner1'] and wall['corner2'] == wall2['corner2']: # Skip if it is the same wall
-                    #    continue
-
                     corner3 = corners[wall2['corner1']]
                     corner4 = corners[wall2['corner2']]
-
 
                     if j == 0:
 
@@ -423,11 +416,11 @@ if cfg.REMOVE_COLLISION==True:
 
                     # Possible collision scenarios
                     # Vertical ->
-                    #             X values are same for either corner1 or corner2
+                    #             X values are same for corners of different walls
                     # Horizontal ->
-                    #             Y values are same for either corner1 or corner2
+                    #             Y values are same for corners of different walls
 
-                    if corner1_X == corner3_X: # All point should have same X value
+                    if corner1_X == corner3_X:  # Vertical collision
 
                         # Two collision scenarios ->
                         # 1)
@@ -454,14 +447,13 @@ if cfg.REMOVE_COLLISION==True:
 
                             continue
 
-                        if corner1_Y > corner3_Y and corner2_Y < corner4_Y: #Scenario 2
+                        if corner1_Y > corner3_Y and corner2_Y < corner4_Y:  #Scenario 2
 
                             willRemove.append(wall2)
                             seen.append(wall2)
                             continue
 
-
-                        if corner2_Y == corner3_Y :
+                        if corner2_Y == corner3_Y:
 
                             seen.append(wall)
                             seen.append(wall2)
@@ -524,15 +516,11 @@ if cfg.REMOVE_COLLISION==True:
         if willRemove[i] not in willRemove[i+1:]:
             willRemoveAgain.append(willRemove[i])
 
-
     for rem in willRemoveAgain:
         reduced_walls.remove(rem)
 
-
     for ad in willAdd:
         reduced_walls.append(ad)
-
-
 
 print("Number of walls: "+str(len(walls)))
 print("Number of walls after reducing: "+str(len(reduced_walls)))
